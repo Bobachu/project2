ad4d1abf03ac2832aa2eb65e5ab43ed3058ed632
amyjosm <amyjosm@gmail.com>
2019-03-04 14:31:20 -0700

    final push before class ended

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static('views/images')); 
 
 // Handlebars
 app.engine(
@@ -23,7 +24,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
