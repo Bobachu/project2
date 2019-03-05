d86e64c0b1c43573c7c8844a949bcbbb75c16d85
amyjosm <amyjosm@gmail.com>
2019-03-05 11:09:46 -0700

    small changes

--- models/searches.js
+++ models/searches.js
@@ -13,14 +13,6 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [1]
             }
-<<<<<<< HEAD
-        },
-        spices: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
         },
         diet: {
             type: DataTypes.STRING,
@@ -43,9 +35,6 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1]
             }
         },
-=======
-        }
->>>>>>> ce0bfeeb93b1f911a55d55e05dd47cc326e04c88
     });
 
     return Searches;
