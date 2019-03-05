f31af1d0914a984d9103177efbbc303901936c97
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-03-04 18:12:59 -0700

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,119 +1,25 @@
-// Get references to page elements
-var $recipeSearch = $("#recipeSearch");
-var $addRecipe = $("#example-description");
-var $contactButton = $("#contactButton");
-// var $submitBtn = $("#submit"); we'll potentially change this to a delete button in the "past searches section" 
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function (example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function () {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function (id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+// var allowedIngredient;
+// var allowedDiet;
+// var allowedAllergy;
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function () {
-  API.getExamples().then(function (data) {
-    var $examples = data.map(function (example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function (event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function () {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function () {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function () {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
-$recipeSearch.on("click", function () {
+// $recipeSearch.on("click", function (event) {
   // whent his button is clicked-take data from front end, convert to variables, create a yummly url with variable 
   // event.preventDefault() can be used to prevent an event's default behavior.
   // Here, it prevents the submit button from trying to submit a form when clicked
-  event.preventDefault();
+  // event.preventDefault();
 
   // Here we grab the text from the input box
-  var recipeSearch = $("#recipeSearch").val().trim();
-  var allowedIngredient
-  var allowedDiet
-  var allowedAllergy
- 
+  // var recipeSearch = $("#recipeSearch").val().trim();
+  // allowedIngredient = $("#allowedIngredient").val().trim();
+  // allowedDiet = $("#allowedDiet").val().trim();
+  // allowedAllergy = $("#allowedAllergy").val().trim();
+  
 
 
   // Here we construct our URL
-  var queryURL =  "http://api.yummly.com/v1/api/recipes?_app_id=1f483524_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&requirePictures=true&" + allowedIngredient + allowedDiet+  allowedAllergy;
+  // var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&requirePictures=true&" + allowedIngredient + allowedDiet + allowedAllergy;
+  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524&_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=broccoli&allowedDiet[]=388^Lacto%20vegetarian";
 
   // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
   // and display it in the div with an id of movie-view
@@ -123,16 +29,23 @@ $recipeSearch.on("click", function () {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-    for (var i = 0; i < response.matches.length; i++){
-      response.matches[i].id
-    }; 
-    
-  
-    $("#searches-view").text(JSON.stringify(response));
 
-    $("#table-div > tbody").append(response);
+  }).then(function (response) {
+    
+    const recipes = response.matches;
+
+    recipes.forEach(function(recipe){
+      console.log(recipe.recipeName);
+      recipe.ingredients.forEach(function(ingredient){
+        console.log("--" + ingredient);
+      });
+    });
+
+  
+    // $("#searchesResults").append(response);
+    // $("#searches-div").toggle(false);
+    // $("#searchesResults").toggle(true);
   });
-});
-$addRecipe.on("click");
-$contactButton.on("#contactButton");
+// });
+// $addRecipe.on("click");
+// $contactButton.on("#contactButton");
