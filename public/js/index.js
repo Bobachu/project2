3b0ba4d8b57a252da455e657dba0eade4efc181b
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-03-05 13:15:58 -0700

    fixed variables

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 // These are global variables being set here so that we can use them in our URL
-const allowedIngredient;
-const allowedDiet;
-const allowedAllergy;
-const recipeSearch;
+var allowedIngredient;
+var allowedDiet;
+var allowedAllergy;
+var recipeSearch;
 
 // When this button is clicked-we take the data from front end, convert to variables, create a yummly url with variable
 $("#recipeSearch").on("click", function(event) {
@@ -26,7 +26,7 @@ $("#recipeSearch").on("click", function(event) {
 
   // Here we construct our URL
 
-  const queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524&_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&allowedIngredient[]=" + allowedIngredient;
+  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524&_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&allowedIngredient[]=" + allowedIngredient;
 
   // Here we send our ajax call to gather the recipes from our API
   $.ajax({
