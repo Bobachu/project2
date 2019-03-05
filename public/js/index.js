e9aca4ca99403f799acb988cb7d88ca6e325146d
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-03-05 13:38:05 -0700

    making the link work

--- public/js/index.js
+++ public/js/index.js
@@ -54,16 +54,16 @@ $("#recipeSearch").on("click", function(event) {
 
       $("#searchesResults").append(
         $("#recipeTitle").html(recipe.recipeName),
-        $("#recipeURL").html(
-          "https://www.yummly.com/recipe/" + recipe.id + "#directions"
-        )
+        $("#recipeURL").attr("href","https://www.yummly.com/recipe/" + recipe.id + "#directions"),
+        $("#recipeURL").html("Recipe Instructions")
+        
       );
     });
 
     // The code below is intended to take the response we get from the code above and append it to our HTML page, replacing our current image.
 
-    // $("#searches-div").toggle(false);
-    // $("#searchesResults").toggle(true);
+    $("#searches-div").toggle(false);
+    $("#searchesResults").toggle(true);
   });
 });
 
