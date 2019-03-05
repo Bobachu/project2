190f322b2bf18f3c75a3905b294fbe555f907c3a
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-03-05 10:23:09 -0700

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,51 +1,66 @@
 
-// var allowedIngredient;
-// var allowedDiet;
-// var allowedAllergy;
+// These are global variables being set here so that we can use them in our URL
+var allowedIngredient;
+var allowedDiet;
+var allowedAllergy;
+var recipeSearch;
 
-// $recipeSearch.on("click", function (event) {
-  // whent his button is clicked-take data from front end, convert to variables, create a yummly url with variable 
-  // event.preventDefault() can be used to prevent an event's default behavior.
-  // Here, it prevents the submit button from trying to submit a form when clicked
-  // event.preventDefault();
+// When this button is clicked-we take the data from front end, convert to variables, create a yummly url with variable 
+$recipeSearch.on("click", function (event) {
 
-  // Here we grab the text from the input box
-  // var recipeSearch = $("#recipeSearch").val().trim();
-  // allowedIngredient = $("#allowedIngredient").val().trim();
-  // allowedDiet = $("#allowedDiet").val().trim();
-  // allowedAllergy = $("#allowedAllergy").val().trim();
-  
+  // This prevents the submit button from refreshing the page when clicked
+  event.preventDefault();
 
+  //   The below code is part of the API search parameters. We can modify our search by looking for chicken recipes that are also gluten free-or modify the search for eggplant recipes that are vegan, etc. I have them commented out because I wasn't sure how to implement them currently
+
+  recipeSearch = $("#recipeSearch").val().trim();
+  allowedIngredient = $("#allowedIngredient").val().trim();
+  allowedDiet = $("#allowedDiet").val().trim();
+  allowedAllergy = $("#allowedAllergy").val().trim();
 
   // Here we construct our URL
-  // var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&requirePictures=true&" + allowedIngredient + allowedDiet + allowedAllergy;
-  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524&_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=broccoli&allowedDiet[]=388^Lacto%20vegetarian";
 
-  // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-  // and display it in the div with an id of movie-view
+  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=1f483524&_app_key=099b7a16023da9a9f8e9fd29763e0aa0&q=" + recipeSearch + "&allowedIngredient[]=" + allowedIngredient;
 
-  // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
-
+  // Here we send our ajax call to gather the recipes from our API
   $.ajax({
     url: queryURL,
     method: "GET"
 
+    // Then we create a function to pull the matches from our response object/array(?)
   }).then(function (response) {
-    
+
+    // we create a variable of recipes which is equal to all of the info in the "matches" array
     const recipes = response.matches;
 
-    recipes.forEach(function(recipe){
+    // The forEach function loactes each element (in this case recipe) in the array
+    recipes.forEach(function (recipe) {
+      // Here we console.log all the recipes in the array and list them by name
       console.log(recipe.recipeName);
-      recipe.ingredients.forEach(function(ingredient){
-        console.log("--" + ingredient);
-      });
+
     });
 
-  
+    // The code below is intended to take the response we get from the code above and append it to our HTML page, replacing our current image. 
     // $("#searchesResults").append(response);
     // $("#searches-div").toggle(false);
     // $("#searchesResults").toggle(true);
   });
-// });
-// $addRecipe.on("click");
+
+});
+
+// These are buttons awaiting their function instructions
+$addRecipe.on("click", function (event) {
+  // This prevents the submit button from refreshing the page when clicked
+  event.preventDefault();
+
+
+
+});
+
+
+
+
+
+
+
 // $contactButton.on("#contactButton");
