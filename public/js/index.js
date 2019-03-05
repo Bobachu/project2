0a066bc499ba7f911c9bad23dd975e91295ed65f
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-03-05 13:40:13 -0700

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -62,8 +62,8 @@ $("#recipeSearch").on("click", function(event) {
 
     // The code below is intended to take the response we get from the code above and append it to our HTML page, replacing our current image.
 
-    // $("#searches-div").toggle(false);
-    // $("#searchesResults").toggle(true);
+    $("#searches-div").toggle(false);
+    $("#searchesResults").toggle(true);
   });
 });
 
